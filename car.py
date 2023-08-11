b430f55ad4bf1d547c0b80aa9aba46b8c7286d58
George Spanodimos <georgespanodimos1@gmail.com>
2023-08-11 21:26:12 +0300

    Final

--- car.py
+++ car.py
@@ -1,10 +1,26 @@
 from abc import ABC, abstractmethod
+from engine.EB import Engine
+from engine.EB import Battery
 
-
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
-
+class Serviceable(ABC):
     @abstractmethod
     def needs_service(self):
         pass
+        
+
+
+class Car(Serviceable):
+    def __init__(self, engine:Engine, battery:Battery):
+        self.engine  = engine
+        self.battery = battery
+
+    
+    def needs_service(self):
+        if self.engine.needs_service():
+         return True
+        if self.battery.needs_service():
+           return True 
+        else:
+           return False
+       
+       
